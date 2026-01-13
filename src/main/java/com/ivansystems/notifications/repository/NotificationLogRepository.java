285e7ed9445d76146b0421f904fb2590de759593
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-01-13 07:45:05 -0700

    Added a repository to be able to obtain the notifications log

---
+++ src/main/java/com/ivansystems/notifications/repository/NotificationLogRepository.java
@@ -0,0 +1,13 @@
+package com.example.demo.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ivansystems.notifications.model.NotificationLog;
+
+import java.util.List;
+
+@Repository
+public interface NotificationLogRepository extends JpaRepository<NotificationLog, Long> {
+    List<NotificationLog> findAllByOrderByTimestampDesc();
+}
