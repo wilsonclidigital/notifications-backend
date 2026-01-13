6024a98fde830e082fb38a9d08eee44dd49406da
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-01-13 09:32:40 -0700

    Relocated the controller to its appropiate package and added the logs endpoint

--- src/main/java/com/ivansystems/notifications/controller/NotificationController.java
+++ src/main/java/com/ivansystems/notifications/controller/NotificationController.java
@@ -1,9 +1,12 @@
-package com.ivansystems.notifications;
+package com.ivansystems.notifications.controller;
+
+import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.ivansystems.notifications.dto.MessageRequest;
+import com.ivansystems.notifications.model.NotificationLog;
 import com.ivansystems.notifications.service.NotificationService;
 
 import jakarta.validation.Valid;
@@ -22,5 +25,10 @@ public class NotificationController {
     public ResponseEntity<String> sendMessage(@Valid @RequestBody MessageRequest request) {
         notificationService.processMessage(request);
         return ResponseEntity.ok("Message processed successfully");
+    }
+
+    @GetMapping("/logs")
+    public ResponseEntity<List<NotificationLog>> getLogHistory() {
+        return ResponseEntity.ok(notificationService.getLogHistory());
     }
 }
