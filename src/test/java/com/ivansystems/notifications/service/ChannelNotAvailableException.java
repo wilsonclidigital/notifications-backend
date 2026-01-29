743fb1943c80672957a68891446f21507c9e3fde
Carlos Ivan Chavez Fuentes <cachavezf@gmail.com>
2026-01-28 20:43:14 -0700

    Relocated classes to their appropiate folder

--- src/test/java/com/ivansystems/notifications/service/ChannelNotAvailableException.java
+++
@@ -1,7 +0,0 @@
-package com.ivansystems.notifications.exception;
-
-public class ChannelNotAvailableException extends RuntimeException {
-    public ChannelNotAvailableException(String message) {
-        super(message);
-    }
-}
